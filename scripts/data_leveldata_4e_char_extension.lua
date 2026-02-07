213728eefa7e9dec708a3d0263aceb3f5a9c6533
Jason Williams <jawawilliams@gmail.com>
2026-02-06 17:21:41 -0800

    Added data for standard AEDU class powers by level

---
+++ scripts/data_leveldata_4e_char_extension.lua
@@ -0,0 +1,122 @@
+-- 
+-- Please see the license.html file included with this distribution for 
+-- attribution and copyright information.
+--
+
+-- New Powers By Level for Standard AWEDU Classes (At-Will, Encounter, Daily, Utility)
+powersGainedByLevel = {
+	[1] = {
+		atwill = 2,
+		encounter = 1,
+		daily = 1,
+		utility = 0
+	},
+	[2] = {
+		atwill = 0,
+		encounter = 0,
+		daily = 0,
+		utility = 1
+	},
+	[3] = {
+		atwill = 0,
+		encounter = 1,
+		daily = 0,
+		utility = 0
+	},
+	[4] = {
+		atwill = 0,
+		encounter = 0,
+		daily = 0,
+		utility = 0
+	},
+	[5] = {
+		atwill = 0,
+		encounter = 0,
+		daily = 1,
+		utility = 0
+	},
+	[6] = {
+		atwill = 0,
+		encounter = 0,
+		daily = 0,
+		utility = 1
+	},
+	[7] = {
+		atwill = 0,
+		encounter = 1,
+		daily = 0,
+		utility = 0
+	},
+	[8] = {
+		atwill = 0,
+		encounter = 0,
+		daily = 0,
+		utility = 0
+	},
+	[9] = {
+		atwill = 0,
+		encounter = 0,
+		daily = 1,
+		utility = 0
+	},
+	[10] = {
+		atwill = 0,
+		encounter = 0,
+		daily = 0,
+		utility = 1
+	},
+	[13] = {
+		atwill = 0,
+		encounter = 1,
+		daily = 0,
+		utility = 0
+	},
+	[15] = {
+		atwill = 0,
+		encounter = 0,
+		daily = 1,
+		utility = 0
+	},
+	[17] = {
+		atwill = 0,
+		encounter = 1,
+		daily = 0,
+		utility = 0
+	},
+	[19] = {
+		atwill = 0,
+		encounter = 0,
+		daily = 1,
+		utility = 0
+	},
+	[22] = {
+		atwill = 0,
+		encounter = 0,
+		daily = 0,
+		utility = 1
+	},
+	[23] = {
+		atwill = 0,
+		encounter = 1,
+		daily = 0,
+		utility = 0
+	},
+	[25] = {
+		atwill = 0,
+		encounter = 0,
+		daily = 1,
+		utility = 0
+	},
+	[27] = {
+		atwill = 0,
+		encounter = 1,
+		daily = 0,
+		utility = 0
+	},
+	[29] = {
+		atwill = 0,
+		encounter = 0,
+		daily = 1,
+		utility = 0
+	},	
+};
